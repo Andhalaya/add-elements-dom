566b42f372109b23ef89e412bd72e437d5c8ae76
Dilia <andhalaya@hotmail.com>
2023-11-08 02:27:25 +0100

    added styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+
+ const element = document.getElementById("agregar");
+ element.addEventListener("click", function(){
+    const name = prompt("Escribe tu pelicula favorita:");
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(name);
+    node.appendChild(textnode)
+    document.getElementById("lista").appendChild(node)
+ });
+
